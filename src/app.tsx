1ee97368181ffd8e74eb4e1417df8afc5d365381
Fernando <fernando_dsouza@icloud.com>
2026-04-09 05:52:35 -0300

    Feat: Criação da tela inicial.

--- src/app.tsx
+++ src/app.tsx
@@ -1,3 +1,24 @@
+import { Plus } from 'lucide-react'
+
+import logo from './assets/logo-in-orbit.svg'
+import letsStart from './assets/lets-start-illustration.svg'
+
 export function App() {
-  return <div>Hello, World!</div>
+  return (
+    <div className="h-screen flex flex-col items-center justify-center gap-8">
+      <img src={logo} alt="In.Orbit Logo" />
+      <img src={letsStart} alt="Let's Start Illustration" />
+      <p className='text-zinc-300 leading-relaxed max-w-80 text-center'>
+        Você ainda não cadastrou nenhuma meta.
+      </p>
+
+      <button 
+        type='button'
+        className='px-4 py-2.5 rounded-lg bg-violet-500 text-violet-50 flex items-center gap-2 text-sm font-medium tracking-tight hover:bg-violet-600'
+        >
+          <Plus className='size-4' />
+          Cadastrar meta
+      </button>
+    </div>
+  )
 }
